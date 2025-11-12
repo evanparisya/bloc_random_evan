abab1a89e6f4f2b6b045251c3487abb746643b66
evanparisya <144124511+evanparisya@users.noreply.github.com>
2025-11-12 23:56:13 +0700

    W12: Jawaban Soal 13

--- lib/random_screen.dart
+++ lib/random_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'random_bloc.dart'; // Impor BLoC yang sudah dibuat
+import 'random_bloc.dart';
 
 class RandomScreen extends StatefulWidget {
   const RandomScreen({super.key});
@@ -9,23 +9,19 @@ class RandomScreen extends StatefulWidget {
 }
 
 class _RandomScreenState extends State<RandomScreen> {
-  // Langkah 11: Buat variabel BLoC
   final _bloc = RandomNumberBloc();
 
-  // Langkah 12: Tutup StreamController saat widget dihapus
   @override
   void dispose() {
     _bloc.dispose();
     super.dispose();
   }
 
-  // Langkah 13: Bangun UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Random Number')),
       body: Center(
-        // StreamBuilder mendengarkan perubahan dari BLoC
         child: StreamBuilder<int>(
           stream: _bloc.randomNumber,
           initialData: 0, // Nilai awal
@@ -38,7 +34,6 @@ class _RandomScreenState extends State<RandomScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        // Ketika tombol ditekan, event dikirim ke BLoC
         onPressed: () => _bloc.generateRandom.add(null),
         child: const Icon(Icons.refresh),
       ),
